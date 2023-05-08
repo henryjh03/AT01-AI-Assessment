e630e56463872298720af4f1694e6607aeb820e2
Henry <henryjh2003@gmail.com>
2023-05-08 15:37:15 +0800

    Edited the Unity version and Added in Player Script Stuff

--- AT01_UnityProject/Assets/Scripts/Player.cs
+++ AT01_UnityProject/Assets/Scripts/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour
     public Node TargetNode { get; private set; }
 
     [SerializeField] private float speed = 4;
+    [SerializeField] private float stoppingDistance;
     private bool moving = false;
     private Vector3 currentDir;
 
@@ -33,10 +34,11 @@ public class Player : MonoBehaviour
         if (moving == false)
         {
             //Implement inputs and event-callbacks here
+            PlayerMoveInput();
         }
         else
         {
-            if (Vector3.Distance(transform.position, TargetNode.transform.position) > 0.25f)
+            if (Vector3.Distance(transform.position, TargetNode.transform.position) > stoppingDistance)
             {
                 transform.Translate(currentDir * speed * Time.deltaTime);
             }
@@ -47,14 +49,16 @@ public class Player : MonoBehaviour
             }
         }
     }
-
+    
     //Implement mouse interaction method here
 
 
-    /// <summary>
-    /// Sets the players target node and current directon to the specified node.
-    /// </summary>
-    /// <param name="node"></param>
+    /* <summary>
+    * Sets the players target node and current directon to the specified node.
+    * </summary>
+    * <param name="node"></param>
+    */
+
     public void MoveToNode(Node node)
     {
         if (moving == false)
@@ -63,6 +67,77 @@ public class Player : MonoBehaviour
             currentDir = TargetNode.transform.position - transform.position;
             currentDir = currentDir.normalized;
             moving = true;
+        }
+    }
+
+    public void PlayerMoveInput()
+    {
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            Debug.Log("left input detected");
+            CheckForNode(-Vector3.right);
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            Debug.Log("right input detected");
+            CheckForNode(Vector3.right);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            Debug.Log("down input detected");
+            CheckForNode(-Vector3.forward);
+        }
+        else if (Input.GetAxis("Vertical") > 0)
+        {
+            Debug.Log("up input detected");
+            CheckForNode(Vector3.forward);
+        }
+    }
+
+    public void PlayerMouseInput(int direction)
+    {
+        /*take in int to determine direction
+        * 0 = North
+        * 1 = East
+        * 2 = South
+        * 3 = West
+        */
+
+        switch (direction)
+        {
+            case 0:
+                CheckForNode(Vector3.forward);
+                return;
+            case 1:
+                CheckForNode(Vector3.right);
+                return;
+            case 2:
+                CheckForNode(-Vector3.forward);
+                return;
+            case 3:
+                CheckForNode(-Vector3.right);
+                return;
+        }
+    }
+
+    public void CheckForNode(Vector3 checkDirection)
+    {
+
+        RaycastHit hit;
+        Node node;
+
+        if (Physics.Raycast(transform.position, checkDirection, out hit, 50f))
+        {
+            if (hit.collider.TryGetComponent<Node>(out node))
+            {
+                Debug.DrawRay(transform.position, checkDirection * 100000, Color.white);
+                MoveToNode(node);
+                Debug.Log("hit " + node.name);
+            }
+        }
+        else
+        {
+            Debug.Log("no valid target");
         }
     }
 }
